4253a3e384bd26904b9ed4edf182265e7292e8e6
Huy Son PHAM <pham.huyson87@gmail.com>
2024-03-23 07:46:30 -0400

    Use "Environment variable"

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"message", "hello!"}
+    return {"message", f"hello! Secret = {os.environ['MY_ENV_VARIABLE']}"}
